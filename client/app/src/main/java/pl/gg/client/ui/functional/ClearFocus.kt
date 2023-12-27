d8690604ddaf5a699b35000fcc36a977f8a8b929
Gerard Gosk <ggosk@een.com>
2023-12-27 15:23:38 +0100

    client - update libs

--- client/app/src/main/java/pl/gg/client/ui/functional/ClearFocus.kt
+++ client/app/src/main/java/pl/gg/client/ui/functional/ClearFocus.kt
@@ -1,15 +1,17 @@
 package pl.gg.client.ui.functional
 
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.platform.LocalFocusManager
-import com.google.accompanist.insets.LocalWindowInsets
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 /***
  * workaround for clearing EditText focus after back pressing
@@ -17,11 +19,13 @@ import androidx.compose.runtime.setValue
  * @see https://issuetracker.google.com/issues/192433071
  * @see https://stackoverflow.com/questions/68389802/how-to-clear-textfield-focus-when-closing-the-keyboard-and-prevent-two-back-pres/68420874#68420874
  */
+@OptIn(ExperimentalLayoutApi::class)
 fun Modifier.clearFocusOnKeyboardDismiss(): Modifier = composed {
     var isFocused by remember { mutableStateOf(false) }
     var keyboardAppearedSinceLastFocused by remember { mutableStateOf(false) }
     if (isFocused) {
-        val imeIsVisible = LocalWindowInsets.current.ime.isVisible
+
+        val imeIsVisible = WindowInsets.isImeVisible
         val focusManager = LocalFocusManager.current
         LaunchedEffect(imeIsVisible) {
             if (imeIsVisible) {
